0204e0f7e4560541c31eb3fa3710b5ab52d4d940
Philip Kuryloski <pkuryloski@pivotal.io>
2019-10-10 19:35:46 +0200

    Add GCP blobstore product sourcing

--- src/foundation_lib/foundation_configuration.clj
+++ src/foundation_lib/foundation_configuration.clj
@@ -42,6 +42,14 @@
 
 (s/def ::pivnet-file-glob string?)
 
+(s/def ::blobstore-bucket string?)
+
+(s/def ::blobstore-product-path string?)
+
+(s/def ::gcs-project-id string?)
+
+(s/def ::gcs-service-account-json string?)
+
 (s/def ::pivnet-api-token string?)
 
 (s/def ::pivnet-disable-ssl boolean?)
@@ -50,30 +58,28 @@
 
 (s/def ::s3-auth-type #{"accesskey" "iam"})
 
-(s/def ::s3-bucket string?)
-
 (s/def ::s3-disable-ssl boolean?)
 
 (s/def ::s3-enable-v2-signing boolean?)
 
 (s/def ::s3-endpoint string?)
 
-(s/def ::s3-product-path string?)
-
 (s/def ::s3-region-name string?)
 
 (s/def ::s3-secret-access-key string?)
 
 (s/def ::source (s/keys :req-un [::pivnet-file-glob]
-                        :opt-un [::pivnet-api-token
+                        :opt-un [::blobstore-bucket
+                                 ::blobstore-product-path
+                                 ::gcs-project-id
+                                 ::gcs-service-account-json
+                                 ::pivnet-api-token
                                  ::pivnet-disable-ssl
                                  ::s3-access-key-id
                                  ::s3-auth-type
-                                 ::s3-bucket
                                  ::s3-disable-ssl
                                  ::s3-enable-v2-signing
                                  ::s3-endpoint
-                                 ::s3-product-path
                                  ::s3-region-name
                                  ::s3-secret-access-key]))
 
